9b6a517bf10ce6b06048d9220e8dc555754d72d3
Milap Narola <milap5322@gmail.com>
2024-08-01 23:16:11 +0530

    Add

--- Lecture-2/Reverse String.js
+++ Lecture-2/Reverse String.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 function reverseString(str) {
     let reversed = '';
@@ -8,16 +7,7 @@ function reverseString(str) {
     return reversed;
 }
 
-=======
-function reverseString(str) {
-    let reversed = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return reversed;
-}
 
->>>>>>> 22f2d13fceddb237500ccd79446cb2c68b953dab
 let originalString = "hello";
 let reversedString = reverseString(originalString);
 console.log(reversedString);
